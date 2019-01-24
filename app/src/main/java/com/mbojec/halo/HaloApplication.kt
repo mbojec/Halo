2df9a727aa9b2a44e1a7aecda32be4c801b580bc
Marek Bojęć <marek.bojec@gmail.com>
2019-01-24 12:23:07 +0100

    Adding dagger and timber library

--- app/src/main/java/com/mbojec/halo/HaloApplication.kt
+++ app/src/main/java/com/mbojec/halo/HaloApplication.kt
@@ -1,10 +1,14 @@
 package com.mbojec.halo
 
 import android.app.Application
+import timber.log.Timber
 
 class HaloApplication : Application(){
 
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
