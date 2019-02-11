888bd9663ff5e246ba555905eb4d03f36fa39e50
Marek Bojęć <marek.bojec@gmail.com>
2019-02-12 00:45:12 +0100

    Loading location from db in simple example in main fragment

--- app/src/main/java/com/mbojec/halo/ui/MainFragment.kt
+++ app/src/main/java/com/mbojec/halo/ui/MainFragment.kt
@@ -3,7 +3,9 @@ package com.mbojec.halo.ui
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
@@ -26,6 +28,7 @@ class MainFragment : Fragment(), Injectable {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+        submitViewModel()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -35,5 +38,16 @@ class MainFragment : Fragment(), Injectable {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController()
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    private fun submitViewModel(){
+        viewModel.location.observe(this, Observer {
+            /**
+             * Przy pierwszym uruchomieniu
+             * @param location
+             * jest null dlatego jest null-check
+             * */
+            it?.let {Toast.makeText(activity, it.latitude.toString(), Toast.LENGTH_SHORT).show()}
+        })
     }
 }
--- app/src/main/java/com/mbojec/halo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/mbojec/halo/viewmodel/MainViewModel.kt
@@ -1,8 +1,18 @@
 package com.mbojec.halo.viewmodel
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel;
 import com.mbojec.halo.HaloApplication
+import com.mbojec.halo.database.LocationEntity
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(haloApplication: HaloApplication): ViewModel() {
+
+    var location: MediatorLiveData<LocationEntity> = MediatorLiveData()
+
+    init {
+        location.addSource(haloApplication.dataRepository.location){
+            this.location.postValue(it)
+        }
+    }
 }
