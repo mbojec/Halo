335425e7c9c898e66ee9f930fbcc28b0796ab82e
Marek Bojęć <marek.bojec@gmail.com>
2019-02-26 22:21:22 +0100

    Changing settings fragment
    Adding list preference and switch preference with proper string's/array's value's

--- app/src/main/java/com/mbojec/halo/ui/SettingsFragment.kt
+++ app/src/main/java/com/mbojec/halo/ui/SettingsFragment.kt
@@ -1,12 +1,11 @@
 package com.mbojec.halo.ui
 
-
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.mbojec.halo.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
 }
